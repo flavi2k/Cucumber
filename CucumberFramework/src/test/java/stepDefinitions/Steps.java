6132c93bbba0493348e017d05f172d15412ab133
Flavian Durigu <flavi2k@gmail.com>
2018-04-02 17:28:48 +0300

    WebDriver Manager

--- CucumberFramework/src/test/java/stepDefinitions/Steps.java
+++ CucumberFramework/src/test/java/stepDefinitions/Steps.java
@@ -1,15 +1,12 @@
 package stepDefinitions;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import dataProviders.ConfigFileReader;
-import managers.FileReaderManager;
 import managers.PageObjectManager;
+import managers.WebDriverManager;
 import pageObjects.CartPage;
 import pageObjects.CheckoutPage;
 import pageObjects.HomePage;
@@ -23,17 +20,12 @@ public class Steps {
 	CheckoutPage checkoutPage;
 	PageObjectManager pageObjectManager;
 	ConfigFileReader configFileReader;
-	
+	WebDriverManager webDriverManager;
 
 	@Given("^user is on Home Page$")
 	public void user_is_on_Home_Page() {
-		configFileReader= new ConfigFileReader();
-		System.setProperty("webdriver.chrome.driver", FileReaderManager.getInstance().getConfigReader().getDriverPath());
-//		System.setProperty("webdriver.chrome.driver", configFileReader.getDriverPath());
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(FileReaderManager.getInstance().getConfigReader().getImplicitlyWait(), TimeUnit.SECONDS);
-//		driver.manage().timeouts().implicitlyWait(configFileReader.getImplicitlyWait(), TimeUnit.SECONDS);
+		webDriverManager = new WebDriverManager();
+		driver = webDriverManager.getDriver();
 		pageObjectManager = new PageObjectManager(driver);
 		homePage = pageObjectManager.getHomePage();
 		homePage.navigateTo_HomePage();	
