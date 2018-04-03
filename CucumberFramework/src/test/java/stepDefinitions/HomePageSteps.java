6f512c515b72eefc7eb9b8daa856e65c6af41a3c
Flavian Durigu <flavi2k@gmail.com>
2018-04-03 10:21:44 +0300

    Sharing test context between Cucumber step definitions

---
+++ CucumberFramework/src/test/java/stepDefinitions/HomePageSteps.java
@@ -0,0 +1,29 @@
+package stepDefinitions;
+
+import cucumber.TestContext;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import managers.PageObjectManager;
+import managers.WebDriverManager;
+import pageObjects.HomePage;
+
+public class HomePageSteps {
+
+	TestContext testContext;
+	HomePage homePage;
+
+	public HomePageSteps(TestContext context) {
+		testContext = context;
+		homePage = testContext.getPageObjectManager().getHomePage();
+	}
+
+	@Given("^user is on Home Page$")
+	public void user_is_on_Home_Page() {
+		homePage.navigateTo_HomePage();
+	}
+
+	@When("^he search for \"([^\"]*)\"$")
+	public void he_search_for(String product) {
+		homePage.perform_Search(product);
+	}
+}
